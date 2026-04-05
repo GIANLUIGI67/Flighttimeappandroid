62d70863c041b4c5dfa3525dee84346530b3bd1b
Gianluigi Grassi <takeair@hotmail.com>
2026-04-05 13:42:17 +0300

    Fix Italian strings duplication

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         minSdk = 24
         targetSdk = 35
 
-        versionCode = 10
-        versionName = "1.0.7"
+        versionCode = 11
+        versionName = "1.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
